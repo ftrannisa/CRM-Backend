e516d60be117bff0f198bf68055e7cd6c6f42eb4
Fitri Annisa Ahlul Jannah <ftrannisaa@gmail.com>
2020-10-19 00:24:47 +0700

    setup schema

---
+++ src/models/crmModels.js
@@ -0,0 +1,27 @@
+import moongose from 'mongoose';
+
+const Schema = moongose.Schema;
+
+export const ContactSchema = new Schema({
+    firstName: {
+        type: String,
+        required: 'Enter a first name'
+    },
+    lastName: {
+        type: String,
+        required: 'Enter a last name'
+    },
+    email: {
+        type: String
+    },
+    company: {
+        type: String
+    },
+    phone: {
+        type: Number
+    },
+    created_dates: {
+        type: Date,
+        default: Date.now
+    }
+});
