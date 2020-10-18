8dc178b9f31d0f7c26e0b7d3e032a396dc59d93b
Fitri Annisa Ahlul Jannah <ftrannisaa@gmail.com>
2020-10-19 00:41:04 +0700

    create GET endpoint

--- src/controllers/crmControllers.js
+++ src/controllers/crmControllers.js
@@ -14,3 +14,13 @@ export const addNewContact = (req, res) => {
     });
 }
 
+export const getContact = (req, res) => {
+
+    Contact.find({}, (err, contact) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(contact);
+    });
+}
+
